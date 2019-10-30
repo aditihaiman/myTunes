0f7638025147550133db26d5288dcc9f3faea6af
aditihaiman <ahaiman00@stuy.edu>
2019-10-30 14:47:06 -0400

    added more test cases

--- linkedlist.h
+++ linkedlist.h
@@ -10,7 +10,7 @@ struct song_node * insert_front(struct song_node *front, char newname[], char ne
 struct song_node * remove_val(struct song_node *front, int data);
 struct song_node * free_list(struct song_node *current);
 struct song_node * findNode(struct song_node *list, char nme[], char artst[]);
-struct song_node * findFirstNode(char artst[]);
+struct song_node * findFirstNode(struct song_node *list, char artst[]);
 struct song_node * randomNode();
 struct song_node * createNode(char newname[], char newartist[]);
 struct song_node * insertAlphabetical(struct song_node *front, char newname[], char newartist[]);
--- main.c
+++ main.c
@@ -7,31 +7,55 @@
 int main(){
     struct song_node *list = NULL;
     printf("TESTING LINKEDLIST\n");
+    
     printf("===========================================================\n");
+    
     printf("Testing empty print_list:\n");
     print_list(list);
     printf("Testing print_list with insert_front:\n");
     char name[100] = "penny lane";
     char artist[100] = "the beatles";
     list = insert_front(list, name, artist);
-    char name1[100] = "hey jude";
-    list = insert_front(list, name1, artist);
-    char name2[100] = "rocky raccoon";
-    list = insert_front(list, name2, artist);
+    strcpy(name, "hey jude");
+    list = insert_front(list, name, artist);
+    strcpy(name, "rocky raccoon");
+    list = insert_front(list, name, artist);
+    strcpy(artist, "abba");
+    strcpy(name, "fernando");
+    list = insert_front(list, name, artist);
+    strcpy(artist, "queen");
+    strcpy(name, "killer queen");
+    list = insert_front(list, name, artist);
     print_list(list);
+    
     printf("===========================================================\n");
+    
     printf("Testing findNode:\n");
     struct song_node *find = NULL;
+    strcpy(artist, "the beatles");
+    strcpy(name, "penny lane");
     find = findNode(list, name, artist);
     printf("looking for [the beatles - penny lane]:\n");
     printf("%s - %s\n", find->artist, find->name);
     printf("looking for [the beatles - hey jude]:\n");
-    find = findNode(list, name1, artist);
+    strcpy(name, "hey jude");
+    find = findNode(list, name, artist);
     printf("%s - %s\n", find->artist, find->name);
     printf("looking for [the beatles - back in the ussr]:\n");
-    char name3[100] = "back in the ussr";
-    find = findNode(list, name3, artist);
+    strcpy(name, "back in the ussr");
+    find = findNode(list, name, artist);
+    
     printf("===========================================================\n");
+    
+    printf("Testing find first node by artist:\n");
+    printf("looking for [the beatles]:\n");
+    find = findFirstNode(list, artist);
+    printf("%s - %s\n", find->artist, find->name);
+    printf("looking for [abba]:\n");
+    strcpy(artist, "abba");
+    find = findFirstNode(list, artist);
+    printf("%s - %s\n", find->artist, find->name);
+
 
 
     
